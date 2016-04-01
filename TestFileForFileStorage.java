21d2feaa38454bbaee2232d3885e2eb8313203a5
immy1996 <iahmed@mail.umw.edu>
2016-04-01 02:31:44 -0400

    Update TestFileForFileStorage.java
    Organized it better

--- TestFileForFileStorage.java
+++ TestFileForFileStorage.java
@@ -11,9 +11,14 @@ public class TestFileForFileStorage{
         Accounts user1 = new Accounts("Charbel", "cmarche", "Sourlemon1");
         Accounts user2 = new Accounts("Nicco", "nbarratto", "ballislife3");
         Accounts user3 = new Accounts("Imran", "iahmad", "iloveminions2");
+        
         user3.addFollowing("nbarratto");
+        
         Tweet tweet = new Tweet("Hello", "3/31/2016", false);
         user2.addNewTweet(tweet);
+        Tweet tweetTest = new Tweet("Imran is better than me in basketball", "4/1/2016", false);
+        uesr1.addNewTweet(tweetTest);
+        
         ArrayList<Accounts> allUsers = new ArrayList(0);
         allUsers.add(user1);
         allUsers.add(user2);
@@ -37,7 +42,7 @@ public class TestFileForFileStorage{
             ListIterator<Tweet> Tweetiter = tweets.listIterator();
             while (Tweetiter.hasNext()){
                 Tweet theTweet = Tweetiter.next();
-                System.out.print(theTweet.getTweetText() + "\n");
+                System.out.print(theTweet.getTweetText() + "\n" + "\n");
             }
         }
     }   

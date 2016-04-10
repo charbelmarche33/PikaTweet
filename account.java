972df3b38fa623b2f8c09641f17568348d393b95
jewishluigi <amudassa@gmail.com>
2016-04-09 23:39:28 -0400

    Added files via upload

---
+++ account.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pikatweet;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Macbook-Air
+ */
+public class account {
+   public account(String name, String username, String password){
+        Username = username;
+        Password = password;
+        Name = name;
+    }
+    /**
+     * Gets the username of this PokeTrainer.
+     * @return String value of the Username
+     */
+    public String getUsername(){
+        return Username;
+    }
+    /**
+     * Gets the password associated with the username.
+     * @return String value of the Password
+     */
+    public String getPassword(){
+        return Password;
+    }
+    public String getName(){
+        return Name;
+    }
+    /**
+     * Sets the Username if one chose to change it.
+     * @param newUsername the username that you want to change to of type String
+     */
+    public void setUsername(String newUsername){
+        Username = newUsername;
+    }
+    /**
+     * Sets the Password if one chose to change it.
+     * @param newPassword the new password of type String
+     */
+    public void setPassword(String newPassword){
+        Password = newPassword;
+    }
+    
+    public void setName(String newName){
+        Name = newName;
+    }
+    /**
+     * Adds a follower, takes in the username of the person to be followed.
+     * @param username the String value of the username of the new person that is being followed.
+     */
+    public void addFollowing(String username){
+        Following.add(username);
+    }
+    /**
+     * Adds a tweet to the list of personal tweets.
+     * @param tweet the tweet to be added of type Tweet.
+     */
+    public void addNewTweet(Tweet tweet){
+        PersonalTweets.add(tweet);
+    }
+    /**
+     * Gets the list of people that user follows and that user should be able to see both pubic and private tweets from.
+     * @return ArrayList of type String that contains all the usernames of the poketrainers that the user follows.
+     */
+    public ArrayList<String> getPeopleFollowed(){
+        return Following;
+    }
+    /**
+     * Gets the tweets that are made by the user.
+     * @return ArrayList of type Tweet that contains all of this user's tweets.
+     */
+    public ArrayList<Tweet> getTweetsMadeByUser(){
+        return PersonalTweets;
+    }
+    private String Username;
+    private String Password;
+    private String Name;
+    private ArrayList<String> Following;
+    private ArrayList<Tweet> PersonalTweets;
+}
